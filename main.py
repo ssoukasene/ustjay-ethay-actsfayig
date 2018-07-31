4f733eacdb28f47bc206c62e81362da0e4e36cc2
Stephen <ssoukasene@gmail.com>
2018-07-30 18:46:40 -0700

    Completed the piglatinizer script. Commiting for Heroku.

--- main.py
+++ main.py
@@ -19,7 +19,37 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    
+    piglatinize_url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+
+
+    # Send a request to https://hidden-journey-62459.herokuapp.com/piglatinize
+    # it should be a POST request, and it should have form data with 'input_text'
+    # of the fact that we scraped. And use the keyword argument 'follow_redirect=False'
+    # when making your request so that you can capture and analyse the redirect response
+
+    # Looks like:
+    # response = requests. .....
+
+    response = requests.post(
+        piglatinize_url,
+        allow_redirects=False,
+        data={'input_text': fact}
+    )
+
+    # print(response.status_code)
+    # print(response.headers)
+    # response.headers is a dictionary that has a key that is 'Location'
+
+    
+
+    # Then, get the 'Location' header from the response.
+    # Looks like:
+    # location_header = response. ...
+    location_header = response.headers['Location']
+
+    return "<a href='{}'>{}</a>".format(location_header, location_header)
 
 
 if __name__ == "__main__":
